a19aebea84bcd19158241cedc5764b130f48b3b2
andress <Andres@Andress-MacBook-Pro.local>
2020-08-09 16:44:58 -0400

    cambios al nav

--- components/header.js
+++ components/header.js
@@ -5,12 +5,9 @@ import {Navbar, FormControl, Button,Nav, Form} from 'react-bootstrap';
 export default function Header(){
     return(
         <Navbar  sticky='top' bg="light" variant="light">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/"><i class="fas fa-home"></i></Navbar.Brand>
             <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/b/datas">Datas</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link><i class="fas fa-home"></i></Nav.Link>
             </Nav>
       </Navbar>
     )
