19f7c24b28492b4fea8b07a2f7743515336425cd
andress <Andres@Andress-MacBook-Pro.local>
2020-08-09 17:27:40 -0400

    agregue vainita jevi4

--- components/layout.js
+++ components/layout.js
@@ -13,11 +13,7 @@ export default function Layout({children}) {
         <>
         
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"></link> 
-        
-
-
-
-
+     
         <Meta />
         <Header />
         <div>
@@ -25,42 +21,18 @@ export default function Layout({children}) {
                 <main>{children}
                                     
                     <div>
-                        <Link href="/"><a><i class="fas fa-chevron-left"></i> Go Back!!</a></Link>
-                        
+                        <Link href="/"><a><i class="fas fa-chevron-left"></i> Go Back!!</a></Link>  
                     </div>
 
-                    
-                    
-                </main>
-                    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
-
-
-
-                    <script>
-                    var firebaseConfig = {
-                        "projectId": "terminal-a343e",
-                        "appId": "1:1009676514852:web:34079a85af81b08336cab1",
-                        "databaseURL": "https://terminal-a343e.firebaseio.com",
-                        "storageBucket": "terminal-a343e.appspot.com",
-                        "locationId": "us-central",
-                        "apiKey": "AIzaSyCObLCmHv7EsOuXc0ToKoTyZtEv4Lk0bMg",
-                        "authDomain": "terminal-a343e.firebaseapp.com",
-                        "messagingSenderId": "1009676514852",
-                        "measurementId": "G-KF2EB2XNN4"
-                    };
-                    firebase.initializeApp(firebaseConfig);
-                    </script>
-                
+                </main>        
             </Container>
-            
         </div>
         <hr/>
+
       <Footer />
       <script src="https://kit.fontawesome.com/df42958a46.js" crossorigin="anonymous"></script>
-     
 
-
-      </>
       
+      </>
       )
     }
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
 
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
