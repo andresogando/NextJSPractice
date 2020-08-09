3d9eae29dec0737965b74b97e4af329527fdd17a
andress <Andres@Andress-MacBook-Pro.local>
2020-08-09 17:23:26 -0400

    agregue vainita jevi3

--- components/layout.js
+++ components/layout.js
@@ -32,7 +32,24 @@ export default function Layout({children}) {
                     
                     
                 </main>
-                
+                    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+
+
+                    <script>
+                    var firebaseConfig = {
+                        "projectId": "terminal-a343e",
+                        "appId": "1:1009676514852:web:34079a85af81b08336cab1",
+                        "databaseURL": "https://terminal-a343e.firebaseio.com",
+                        "storageBucket": "terminal-a343e.appspot.com",
+                        "locationId": "us-central",
+                        "apiKey": "AIzaSyCObLCmHv7EsOuXc0ToKoTyZtEv4Lk0bMg",
+                        "authDomain": "terminal-a343e.firebaseapp.com",
+                        "messagingSenderId": "1009676514852",
+                        "measurementId": "G-KF2EB2XNN4"
+                    };
+                    firebase.initializeApp(firebaseConfig);
+                    </script>
                 
             </Container>
             
